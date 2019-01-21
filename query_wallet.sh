bc8de5bdb8dd7646af58c4cbbe1d44253ca03583
Hayden <hdnsimpson@gmail.com>
2019-01-22 12:50:31 +1300

    Added wallet api calls functionality
    The user can now use api calls for more performance instead of webdriving

---
+++ query_wallet.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# This script will get data from our wallet
+
+# Get wallet data
+data="$(wget -qO- https://www.navexplorer.com/address/NgZNDCxF8DGzxjPi5NiUcHGkKhtYS1LY51/tx.json)"
+
+# Get amount of donations made
+donations="$(echo "${data}" | tr ',' $'\n' | grep -c RECEIVE)"
+
+# Write to donations file
+echo "${donations}" > /home/pi/Documents/donations.txt
+perl -pi -e 'chomp if eof' /home/pi/Documents/donations.txt
+
+# Get amount donated
+received_amounts="$(echo "${data}" | tr ',' $'\n' | grep '"received":' | cut -d':' -f 2)"
+
+# Write transaction history to file
+echo "${received_amounts}" > /home/pi/Documents/txn_history.txt
+perl -pi -e 'chomp if eof' /home/pi/Documents/txn_history.txt
+
+# Sum donations
+total_received="$(LC_NUMERIC="C" awk '{sum += $1} END {print sum}' txn_history.txt)"
+
+# Calculate coffees donated
+coffees="$(echo $(("${total_received}" / 5)))"
+
+# Write coffees donated
+echo "${coffees}" > /home/pi/Documents/coffees_donated.txt
+perl -pi -e 'chomp if eof' /home/pi/Documents/coffees_donated.txt
